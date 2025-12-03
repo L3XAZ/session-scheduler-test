69acc55f3f9a995b04fa81753aaf592157bc9909
Oleksii Sachenko <alexsatchenko@gmail.com>
2025-12-03 16:26:48 +0700

    chore: update linting, formatting and build configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,30 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
     ...nextVitals,
     ...nextTs,
+
     {
         plugins: {
             '@typescript-eslint': tsPlugin,
             react: reactPlugin,
             'react-hooks': reactHooksPlugin,
+            import: importPlugin,
+        },
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
 
         languageOptions: {
@@ -26,7 +37,9 @@ export default defineConfig([
         rules: {
             'react/react-in-jsx-scope': 'off',
             'react/jsx-uses-react': 'off',
+
             '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
             'react-hooks/rules-of-hooks': 'error',
             'react-hooks/exhaustive-deps': 'warn',
 
